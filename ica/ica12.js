18623c2c555c16555dfaf1022b0d18658c05f817
Samantha Duran <sadu6223@colorado.edu>
2024-04-21 23:54:07 -0600

    new wa13 stuff

--- ica/ica12.js
+++ ica/ica12.js
@@ -1,4 +1,41 @@
 const btn = document.querySelector("#js-new-quote");
 btn.addEventListener('click', getQuote);
 
-const answerBtn = document.querySelector("#js-tw
+const answerBtn = document.querySelector("#js-tweet");
+btn.addEventListener
+
+const answer
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote() {
+    // console.log("Test");
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+
+        const json = await response.json();
+        console.log(json['question']);
+        displayQuote(json['question']);
+        console.log(json['answer']);
+        answer = json['answer'];
+    } catch (err) {
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector
+    ("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+function getAnswer() {
+    console.log(2212);
+}
+    answerText.textContent = answer;
+
+
+    getQuote
---
+++ wa/wa13.js
@@ -0,0 +1,75 @@
+const workers {
+    "employees": [
+        {
+            "firstName": "Sam",
+            "department": "Tech",
+            "designation": 
+            "salary": 
+
+
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Question 1");
+console.log(workers);
+
+const company = {
+    "companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees": workers,
+}
+console.log("Question 2");
+console.log(company);
+
+company.employees.push({})
+company.employees.employees[3].firstName = "Anna";
+company.employees.employees[3].department = "Tech";
+company.employees.employees[3].designation = "Executive";
+company.employees.employees[3].salary = 25600;
+company.employees.employees[3].raiseEligible = false;
+
+let total = 0;
+for (x in company.employees.employees) {
+    total += company.employees.employees[x].salary;
+}
+
+console.log("Question 4");
+console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+const array = ["Anna", "Sam"];
+
+for (x in company.employees.employees) {
+    if ((company.employees.employees[x].firstName == "Anna" || "Sam") {
+        company 
+    }
+}
+
+
+
